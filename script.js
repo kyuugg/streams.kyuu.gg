afc11e041afcba63715bcc562fab3aeb82720ecc
ayukyuu <arya72480@gmail.com>
2026-07-05 19:47:09 +0200

    adjust class names for old script to work again

--- script.js
+++ script.js
@@ -1,24 +1,38 @@
 const nav = document.querySelector("#buttons-nav");
-const input = document.querySelector("#sidebar-search");
+const input = document.querySelector("#sidebar-input");
 const streamFrame = document.querySelector("#stream-frame");
 
 function generateSource(streamer) {
-    return `https://player.twitch.tv/?channel=${streamer}&parent=kyuugg.github.io`;
+    return `https://player.twitch.tv/?channel=${streamer}&parent=127.0.0.1`;
 }
 
 input.addEventListener("keydown", (event) => {
-    if (event.key === "Enter") {
-        const newButton = document.createElement("button");
-        newButton.className = "sidebar-item";
-        newButton.textContent = input.value;
-        nav.append(newButton);
+    if (event.key === "Enter" && input.value.trim()) {
+        const buttonDiv = document.createElement("div");
+        buttonDiv.className = "sidebar-button-container";
+
+        const streamerButton = document.createElement("button");
+        streamerButton.className = "sidebar-streamer-button";
+        streamerButton.textContent = input.value;
+
+        const deleteButton = document.createElement("button");
+        deleteButton.className = "sidebar-delete-button";
+        deleteButton.innerHTML = "&#128465;";
+
+        buttonDiv.append(streamerButton);
+        buttonDiv.append(deleteButton);
+
+        nav.append(buttonDiv);
+
         input.value = "";
     }
 });
 
 nav.addEventListener("click", (event) => {
-    if (event.target.matches(".sidebar-item")) {
+    if (event.target.matches(".sidebar-streamer-button")) {
         const streamer = event.target.textContent;
         streamFrame.src = generateSource(streamer);
+    } else if (event.target.matches(".sidebar-delete-button")) {
+        event.target.parentElement.remove();
     }
 });
